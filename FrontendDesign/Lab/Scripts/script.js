35f392cc5c4af7494ccc5e0585e0644a13b0e46d
adtek98 <92858364+adtek98@users.noreply.github.com>
2022-05-06 14:50:58 +0200

    drag and drop, now deletes the old container if empty
    todo: create new container if a local is dropped outside a container

--- FrontendDesign/Lab/Scripts/script.js
+++ FrontendDesign/Lab/Scripts/script.js
@@ -1,13 +1,45 @@
-function allowDrop(ev) {
-    ev.preventDefault();
-  }
-  
-  function drag(ev) {
-    ev.dataTransfer.setData("text", ev.target.id);
-  }
-  
-  function drop(ev) {
-    ev.preventDefault();
-    var data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
-  }
+const draggables = document.querySelectorAll('.draggable');
+const containers = document.querySelectorAll('.container');
+
+var previousContainer;
+
+draggables.forEach(draggable => {
+  draggable.addEventListener('dragstart', () => {
+    draggable.classList.add('dragging');
+    previousContainer = draggable.parentElement;
+  })
+
+  draggable.addEventListener('dragend', () => {
+    draggable.classList.remove('dragging');
+
+    if(draggable.parentElement.classList[0] == 'container'){
+      draggable.firstElementChild.classList.add('menuitemSm');
+      draggable.firstElementChild.classList.add('mx-1');
+      draggable.firstElementChild.firstElementChild.classList.remove('mt-3');
+    }
+
+    if(previousContainer.childElementCount == 1){
+      previousContainer.remove()
+    }
+  })
+})
+
+
+containers.forEach(container => {
+
+  container.addEventListener('dragover', e => {
+    e.preventDefault();
+    const draggable = document.querySelector('.dragging');
+    container.appendChild(draggable);
+  })
+
+  container.addEventListener('dragend', () => {
+    
+    container.parentElement.firstElementChild.removeAttribute('draggable');
+    //container.parentElement.firstElementChild.firstElementChild.removeAttribute('draggable');
+    container.parentElement.firstElementChild.classList.remove('draggable');
+    container.parentElement.firstElementChild.firstElementChild.classList.add('undraggable');
+    console.log(container.parentElement.firstElementChild.firstElementChild)
+  })
+})
+
