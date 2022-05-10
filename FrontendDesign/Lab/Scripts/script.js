22002522cc1860402b2a1775f3cd409ea77a0b43
adtek98 <92858364+adtek98@users.noreply.github.com>
2022-05-10 23:17:59 +0200

    drag and drop update

--- FrontendDesign/Lab/Scripts/script.js
+++ FrontendDesign/Lab/Scripts/script.js
@@ -1,5 +1,7 @@
 const draggables = document.querySelectorAll('.draggable');
 const containers = document.querySelectorAll('.container');
+const childcontainers = document.querySelectorAll('.childcontainer');
+//const typecontainers = document.querySelectorAll('.typecontainer')
 
 var previousContainer;
 
@@ -12,34 +14,78 @@ draggables.forEach(draggable => {
   draggable.addEventListener('dragend', () => {
     draggable.classList.remove('dragging');
 
-    if(draggable.parentElement.classList[0] == 'container'){
-      draggable.firstElementChild.classList.add('menuitemSm');
-      draggable.firstElementChild.classList.add('mx-1');
-      draggable.firstElementChild.firstElementChild.classList.remove('mt-3');
-    }
+    // Makes parent element undraggable if it has childrens
+    MakeUndraggable(draggable);
 
-    if(previousContainer.childElementCount == 1){
-      previousContainer.remove()
-    }
+    // Makes parent element draggable if it has no childrens
+    MakeDraggable(previousContainer);
+
+    // makes the icon smaller if its in a child container
+    ConvertToChild(draggable)
+
+    // makes the icon bigger if its in a parent container
+    ConvertToParent(draggable);
   })
 })
 
 
 containers.forEach(container => {
-
   container.addEventListener('dragover', e => {
     e.preventDefault();
     const draggable = document.querySelector('.dragging');
-    container.appendChild(draggable);
-  })
 
-  container.addEventListener('dragend', () => {
-    
-    container.parentElement.firstElementChild.removeAttribute('draggable');
-    //container.parentElement.firstElementChild.firstElementChild.removeAttribute('draggable');
-    container.parentElement.firstElementChild.classList.remove('draggable');
-    container.parentElement.firstElementChild.firstElementChild.classList.add('undraggable');
-    console.log(container.parentElement.firstElementChild.firstElementChild)
+    if(container.childElementCount == 0){
+      container.appendChild(draggable);
+      //container.parentElement.querySelector('.childcontainer').classList.remove('childcontainer');
+    }
   })
 })
 
+childcontainers.forEach(childcontainer => {
+  childcontainer.addEventListener('dragover', e => {
+    e.preventDefault();
+    const draggable = document.querySelector('.dragging');
+    if(childcontainer.parentElement.firstElementChild.childElementCount == 1 && previousContainer.parentElement.id != childcontainer.parentElement.id){
+      childcontainer.appendChild(draggable);
+    }
+    
+  })
+})
+
+
+
+
+function MakeUndraggable(draggable) {
+  if(draggable.parentElement.childElementCount == 1 && draggable.parentElement.classList[0] == 'childcontainer'){
+    draggable.parentElement.parentElement.firstElementChild.firstElementChild.classList.remove('draggable');
+    draggable.parentElement.parentElement.firstElementChild.firstElementChild.classList.add('undraggable');
+    draggable.parentElement.parentElement.firstElementChild.firstElementChild.removeAttribute('draggable')
+  }
+  else return;
+}
+
+function MakeDraggable(previousContainer){
+  if(previousContainer.classList[0] == 'childcontainer' && previousContainer.childElementCount == 0){
+    previousContainer.parentElement.firstElementChild.firstElementChild.classList.add('draggable');
+    previousContainer.parentElement.firstElementChild.firstElementChild.classList.remove('undraggable');
+    previousContainer.parentElement.firstElementChild.firstElementChild.setAttribute('draggable', 'true')
+  }
+  else return;
+}
+
+function ConvertToChild(draggable){
+  if(draggable.parentElement.classList[0] == 'childcontainer'){ 
+    draggable.firstElementChild.classList.add('menuitemSm');
+    draggable.firstElementChild.classList.add('m-1');
+    draggable.firstElementChild.firstElementChild.classList.remove('mt-3');
+  }
+  else return;
+}
+
+function ConvertToParent(draggable){
+  if(draggable.parentElement.classList[0] == 'container' ){ 
+    draggable.firstElementChild.classList.remove('menuitemSm');
+    draggable.firstElementChild.classList.remove('m-1');
+    draggable.firstElementChild.firstElementChild.classList.add('mt-3');
+  }
+}
